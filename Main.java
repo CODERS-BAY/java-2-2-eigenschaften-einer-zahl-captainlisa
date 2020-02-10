c87ba1005c677829670af689064549ab34f02169
Lisa-Maria Plakolb <lisa.plakolb96@gmx.at>
2020-02-10 11:47:36 +0100

    Add Solution

---
+++ Main.java
@@ -0,0 +1,35 @@
+package com.codersbay;
+
+import java.util.Scanner;
+public class Main {
+
+    public static void main(String[] args) {
+
+        int luckyNumber = 13;
+
+        System.out.println("My lucky number is " + luckyNumber);
+        System.out.println("Enter a number:");
+
+        Scanner scan = new Scanner(System.in);
+        int number = scan.nextInt();
+
+        if (number == luckyNumber) {
+            System.out.println(number + " is my lucky number.");
+        }
+
+        if (number % 10 == 0) {
+            System.out.println(number + " is a round number.");
+        }
+
+        if (number % 2 == 0) {
+            System.out.println(number + " is an even number.");
+        } else {
+            System.out.println(number + " is an odd number.");
+        }
+
+        if (number > 9 && number < 100) {
+            System.out.println(number + " has two digits.");
+        }
+
+    }
+}
